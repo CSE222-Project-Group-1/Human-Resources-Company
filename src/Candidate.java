56fabc852f3e8f1e1a5f8c9ca4363bd0677a8aa5
Brnslmz <80854458+Brnslmz@users.noreply.github.com>
2021-06-10 00:26:15 +0300

    Biraz javadoc eklendi

--- src/Candidate.java
+++ src/Candidate.java
@@ -5,6 +5,11 @@ public class Candidate {
 	private String password;
 	private String statue;
 	private CvClass Mycv;
+	/**Constructor
+	* @param String name
+	* @param String password
+	* @param CvClass mycv
+ 	*/
 	public Candidate(String name, String password, CvClass mycv) {
 		super();
 		this.name = name;
@@ -23,6 +28,7 @@ public class Candidate {
 		//TODO
 		return false;
 	}
+	
 	public CvClass	Giveinformation() {
 		return Mycv;
 	}
@@ -34,12 +40,18 @@ public class Candidate {
 	}
 	public boolean SetStatusToOpenWork() {
 		this.statue="Open To Work";
+		//TODO
 		return true;
 	}
 	public int RatingTheOffer() {
 		//TODO
 		return -1;
 	}
+	/**
+	*Changes Candidates Password
+	*@param String newPassword
+	* @returns Boolen 
+	*/
 	public boolean ChangePassword(String newPassword) {
 		setPassword(newPassword);
 		return true;
@@ -49,18 +61,33 @@ public class Candidate {
 		//TODO
 		return aString.toString();
 	}
+	/**Returns Candidates Name
+	*@retuns String
+	*/
 	public String getName() {
 		return name;
 	}
+	/**Sets Candidates Name
+	*@param String name
+	*/
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**Returns Candidates Password
+	*@retuns String
+	*/
 	public String getPassword() {
 		return password;
 	}
+	/**Sets Candidates Password
+	*@param String password
+	*/
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	/**Sets Candidates CV
+	*@param CvClass mycv
+	*/
 	public void setMycv(CvClass mycv) {
 		Mycv = mycv;
 	}
