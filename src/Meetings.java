890ef8ab77559f4670f1a082cb88edd14f4927bf
Refik Orkun Arslan <32647679+refikorkunarslan@users.noreply.github.com>
2021-06-11 20:26:31 +0300

    Update Meetings.java

--- src/Meetings.java
+++ src/Meetings.java
@@ -1,7 +1,9 @@
 package src;
 
+import java.util.Calendar;
+import java.util.*;
 public class Meetings{
-	
+
 	public Meetings(String date, Candidate candidate, Company company, String time, int meetingRate) {
 		super();
 		this.date = date;
@@ -18,9 +20,7 @@ public class Meetings{
 	public String getDate() {
 		return date;
 	}
-	public void setDate(String date) {
-		this.date = date;
-	}
+	public void setDate(String date) { this.date = date; }
 	public Candidate getCandidate() {
 		return candidate;
 	}
@@ -45,5 +45,89 @@ public class Meetings{
 	public void setMeetingRate(int meetingRate) {
 		MeetingRate = meetingRate;
 	}
-	
-}
+	public void printCalender()
+	{
+		Calendar calendar = Calendar.getInstance();
+		int year = calendar.get(Calendar.YEAR);
+
+
+		int day =calendar.get(Calendar.SECOND);
+		int dayCounter = day;
+		int nbrOfDays = 0;
+		String  monthx = "";
+		for (int month= 1; month <=12; month++)
+		{
+
+			switch (month)
+			{
+				case 1: monthx = "January";
+					nbrOfDays = 31;
+					break;
+				case 2: monthx = "February";
+					if ( year % 4 == 0 && year % 100 !=0 || year % 400 == 0)
+					{
+						nbrOfDays = 29;
+						break;
+					}
+					else
+					{  nbrOfDays = 28;
+						break;
+					}
+				case 3: monthx = "March";
+					nbrOfDays = 31;
+					break;
+				case 4: monthx = "April";
+					nbrOfDays = 30;
+					break;
+				case 5: monthx = "May";
+					nbrOfDays = 31;
+					break;
+
+				case 6: monthx = "June";
+					nbrOfDays = 30;
+					break;
+				case 7: monthx = "July";
+					nbrOfDays = 31;
+					break;
+				case 8: monthx = "August";
+					nbrOfDays = 31;
+					break;
+				case 9: monthx = "September";
+					nbrOfDays = 30;
+					break;
+				case 10: monthx = "October";
+					nbrOfDays = 31;
+					break;
+				case 11: monthx = "November";
+					nbrOfDays = 30;
+					break;
+				case 12: monthx = "December";
+					nbrOfDays = 31;
+					break;
+			}
+
+			System.out.printf("%15s %d  \n", monthx , year);
+			System.out.println("----------------------------");
+			System.out.printf("%s %s %s %s %s %s %s\n ", "Sun","Mon","Tue", "Wed", "Thu","Fri","Sat");
+
+			for (int space =1; space<=day; space++)
+			{
+				System.out.printf("%4s", "    ");
+			}
+			for (int i = 1; i <=nbrOfDays; i++)
+			{
+				dayCounter++;
+				if ( dayCounter% 7==0)
+					System.out.printf("%- 4d\n", i);
+				else
+					System.out.printf("%-4d", i);
+
+			}
+			day = (day + nbrOfDays)%7;
+
+			System.out.println();
+
+		}
+
+		}
+	}
