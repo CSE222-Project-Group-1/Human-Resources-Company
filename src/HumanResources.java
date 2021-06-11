7b4c6416d38bd929572bed598416734ba004c4e5
Süleyman Gölbol <64928475+sglbl@users.noreply.github.com>
2021-06-11 22:01:39 +0300

    change

--- src/HumanResources.java
+++ src/HumanResources.java
@@ -1,60 +1,68 @@
 package src;
 
-import java.util.ArrayList;
+import java.util.*;
 
-public class HumanResources <E extends Comparable<E>> implements Comparable<HumanResources<E>>{
+public class HumanResources extends Users implements Comparable<HumanResources>{
+    private int userID;
 	private String name;
 	private String password;
 	private static HRC hrc=null;
 	
-	public HumanResources(String name, String password, ArrayList<Candidate> candidates) {
-		super();
-		this.name = name;
-		this.password = password;
+	public HumanResources(int userID, String name, String password, ArrayList<Candidate> candidates) {
+		super(userID, name, password, Users.HUMAN_SOURCES);
 	}
 
-	public boolean loginSystem() {
+	public boolean loginSystem(String name,String password) {
+		if (this.name.equals(name) && this.password.equals(password))
+			return true;
 		return false;
 	}
+
 	public void SeeCompanyRequest() {
 		
 	}
+
 	public Candidate GiveOfferToCandidate() {
 		Candidate aCandidate = null;
 		return aCandidate;
 	}
+
 	public void SuggestCandidateToCompany() {
 		
 	}
+
 	public boolean CompareRequests() {
 		return false;
 	}
+    
 	public boolean DeleteCandidate() {
 		return false;
 	}
+
 	public boolean ArrangeMeeting() {
 		return false;
 	}
+
 	public String toString() {
 		StringBuilder aString =new StringBuilder();
 		return aString.toString();
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	@Override
-	public int compareTo(HumanResources<E> o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 	
+    
 }
