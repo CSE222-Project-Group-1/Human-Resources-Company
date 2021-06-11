d4ac2ef2db6c37d256b271d1d86e9fa1199e488e
Enis Yalçın <enisyalcin@users.noreply.github.com>
2021-06-11 22:51:20 +0300

    Update HRC
    Add getCandidate

--- src/HRC.java
+++ src/HRC.java
@@ -10,7 +10,6 @@ import src.book_implementation.SkipList;
 
 @SuppressWarnings("unused")
 public class HRC{
-		
 		public static int  USERS_ID=0;
 		private AVLTree<HumanResources> humanResources; // AVL
 		private SkipList<Company> company;
@@ -18,7 +17,7 @@ public class HRC{
 		private ArrayList<Meetings> meetings;
 		private Admin admin=null;
 		
-		
+
 		public SkipList<Company> getCompany() { return company; }
 
 		public AVLTree<HumanResources> getHumanResources() { return humanResources; }
@@ -27,7 +26,9 @@ public class HRC{
 
 		public Admin getAdmin(){	return admin;	}
 
-		public Candidate getCandidate(int id){	return candidate.get(id);	}
+		public Candidate getCandidateID(int id){	return candidate.get(id);	}
+		
+		public NavigableMap<Integer,Candidate> getCandidate(){	return candidate;	}
 
 
 		public Company createCompany(int ID,String name, String password, ArrayList<AdvertiseClass> advertises,
@@ -44,16 +45,12 @@ public class HRC{
 			return createCompany(USERS_ID++,name,password,advertises,companySector,numberOfEmployees,socialRights,address,ratings,ratingsOrt);
 		}
 
-		public HumanResources createHumanResources(int ID,String name, String password, ArrayList<Candidate> candidates){
-			return new HumanResources(ID,name,password,candidates,this);
+		public HumanResources createHumanResources(int ID,String name, String password){
+			return new HumanResources(ID,name,password,this);
 		}
 
-		public HumanResources createHumanResources(String name, String password, ArrayList<Candidate> candidates){
-			return createHumanResources(USERS_ID++,name,password,candidates);
+		public HumanResources createHumanResources(String name, String password){
+			return createHumanResources(USERS_ID++,name,password);
 		}
-		
-		public void setAdmin(Admin admin) {
-		    	this.admin = admin;
-		}
-		
+
 }
