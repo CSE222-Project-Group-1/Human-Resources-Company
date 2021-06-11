c1578c1a9f1ba0636964193d6725255c9bf0d034
Enis Yalçın <enisyalcin@users.noreply.github.com>
2021-06-11 22:05:10 +0300

    Update HRC
    Add Create Methods

--- src/HRC.java
+++ src/HRC.java
@@ -9,20 +9,51 @@ import src.book_implementation.SkipList;
 
 
 @SuppressWarnings("unused")
-public class HRC<E extends Comparable<E>>{
+public class HRC{
 		
-		private AVLTree<HumanResources<E>> humanResources; // AVL
-		private SkipList<Company> company; 
+		public static int  USERS_ID=0;
+		private AVLTree<HumanResources> humanResources; // AVL
+		private SkipList<Company> company;
 		private NavigableMap<Integer,Candidate> candidate= new TreeMap<>();
 		private ArrayList<Meetings> meetings;
 		private Admin admin=null;
 		
 		
-        public void setAdmin(Admin admin) {
-            this.admin = admin;
-        }
 		public SkipList<Company> getCompany() { return company; }
+
+		public AVLTree<HumanResources> getHumanResources() { return humanResources; }
+
+		public ArrayList<Meetings> getMeetings() { return meetings; }
+
+		public Admin getAdmin(){	return admin;	}
+
+		public Candidate getCandidate(int id){	return candidate.get(id);	}
+
+
+		public Company createCompany(int ID,String name, String password, ArrayList<AdvertiseClass> advertises,
+									 String companySector,int numberOfEmployees,
+									 ArrayList<String> socialRights, String address,
+									 ArrayList<Integer> ratings, double ratingsOrt){
+			return new Company(ID,name,password,advertises,companySector,numberOfEmployees,socialRights,address,ratings,ratingsOrt,this);
+		}
+
+		public Company createCompany(String name, String password, ArrayList<AdvertiseClass> advertises,
+									 String companySector,int numberOfEmployees,
+									 ArrayList<String> socialRights, String address,
+									 ArrayList<Integer> ratings, double ratingsOrt){
+			return createCompany(USERS_ID++,name,password,advertises,companySector,numberOfEmployees,socialRights,address,ratings,ratingsOrt);
+		}
+
+		public HumanResources createHumanResources(int ID,String name, String password, ArrayList<Candidate> candidates){
+			return new HumanResources(ID,name,password,candidates,this);
+		}
+
+		public HumanResources createHumanResources(String name, String password, ArrayList<Candidate> candidates){
+			return createHumanResources(USERS_ID++,name,password,candidates);
+		}
 		
-		
+		public void setAdmin(Admin admin) {
+		    	this.admin = admin;
+		}
 		
 }
